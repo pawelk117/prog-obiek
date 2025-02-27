c3d764c6c59f549db6dc747687cba7895eb6e90c
student <pawelkozuchowski117@gmail.com>
2025-02-27 16:07:05 +0100

    dodano Point

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Point p = new Point();
+        p.x = 0.4;
+        p.y = 3.0;
+        System.out.println("("+p.x+", "+p.y+")");
+        System.out.println("P: "+p.toString);
     }
 }
---
+++ svg/src/Point.java
@@ -0,0 +1,6 @@
+public class Point {
+    public double x, y;
+    public String toString(){
+        return "("+p.x+", "+p.y+")";
+    }
+}
