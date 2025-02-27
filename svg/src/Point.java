973995eb2dd365a87d26c194de100f7eff82e3d4
student <pawelkozuchowski117@gmail.com>
2025-02-27 16:20:35 +0100

    dodano Point

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -3,4 +3,17 @@ public class Point {
     public String toString(){
         return "("+p.x+", "+p.y+")";
     }
+    public String toSVG(){
+        return "<circle r=\"45\" cx=\""+p.x+"\" cy=\""+p.y+"\" fill=\"red\" />";
+    }
+    public void translate(double dx, double dy){
+        x += dx;
+        y += dy;
+    }
+    public Point translated(double dx, double dy){
+        Point newPoint = new Point();
+        newPoint.x = dx;
+        newPoint.y = dy;
+        return newPoint;
+    }
 }
